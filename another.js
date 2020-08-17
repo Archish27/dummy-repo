50bc0189a027e6c5912f5d29d42faa395dd51846
Archish Thakkar <archishthakkar@gmail.com>
2020-08-17 11:31:49 +0530

    Update another.js

--- another.js
+++ another.js
@@ -16,9 +16,11 @@ var driver = new webdriver.Builder()
   .withCapabilities(capabilities)
   .build();
 
-driver.get('http://localhost:8099').then(function () {
+driver.get('http://google.com').then(function () {
   driver.getTitle().then(function (title) {
     console.log(title);
-    driver.quit();
+    setTimeout(() => {
+      driver.quit();
+    },5000);  
   });
 });
